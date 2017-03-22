36a32c05bdb9bfd1ee2b0785b7921b3df6ca09bb
Ryan Daley <rpdaley@gmail.com>
2017-03-22 16:58:15 +0000

    hover states done?

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,19 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// $( document ).ready(function() {
+//   $( ".tweet" ).mouseenter(function() {
+//     console.log(this)
+//   });
+// });
+
+$( document ).ready(function() {
+  $(this).find( ".footerButtons" ).hide();
+  $( ".tweet" ).hover(function() {
+    $(this).css("opacity", "1");
+    $(this).find( ".footerButtons" ).show();
+  }, function() {
+    $(this).css("opacity", "0.5");
+    $(this).find( ".footerButtons" ).hide();
+  }
+)});
